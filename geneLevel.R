2bd250c4ccb78f02c3276f4262b0ba6295e8ef12
powerhorse1986 <47227800+powerhorse1986@users.noreply.github.com>
2020-06-15 18:57:15 -0500

    Updated
    1. added function for generating varianceStabilizingTransformation normalized data and store them into local
    2. pre-filter data by treatment groups

--- geneLevel.R
+++ geneLevel.R
@@ -16,8 +16,8 @@ library(DESeq2)
 library(rhdf5)
 library(EnsDb.Hsapiens.v75)
 
-samples <- read.table("/home/lima/Project/simulation/Comparision/samples.txt", header = TRUE)
-samples
+high <- read.table("/home/lima/Project/simulation/Comparision/high.txt", header = TRUE)
+high
 
 ###########################################################
 # kallisto
@@ -30,49 +30,127 @@ tx2gene <- df[, 8 : 7]  # tx ID, then gene ID
 ###########################################################
 # kallisto TSV files for real data
 ###########################################################
-files <- file.path(kallPath, "Real", samples$GeneID, "abundance.tsv")
-names(files) <- samples$GeneID
+files <- file.path(kallPath, "Real", high$GeneID, "abundance.tsv")
+names(files) <- high$GeneID
 # some problems with rhdf5 packages
 #txi.kall <- tximport(files, type = "kallisto", txOut = TRUE)
 txi.kall.tsv <- tximport(files, type = "kallisto", dropInfReps=TRUE, tx2gene = tx2gene, ignoreTxVersion = TRUE, ignoreAfterBar = TRUE)
+txi.kall.tsv$length <- txi.kall.tsv$length + 0.5
+# txi.kall.tsv$counts <- log2(txi.kall.tsv$counts + 1)
 head(txi.kall.tsv$counts)
-dds <- DESeqDataSetFromTximport(txi.kall.tsv, samples, ~1)
+dds <- DESeqDataSetFromTximport(txi.kall.tsv, high, ~1)
 
 # performe a rlog/varianceStabilizingTransformation
 rld <- varianceStabilizingTransformation(dds, blind = FALSE)
-
+# type(rld)
 # convert counts to a dataframe and save it into csv
-rld_df <- data.frame(assay(rld))
-write.csv(rld_df, file.path(kallPath, "Real", "KallistoReadCounts.csv"))
+# rld_df <- data.frame(assay(rld))
+df <- data.frame(assay(dds))
+write.csv(df, file.path("/home/lima/Project/simulation/Comparision/high", "KallistoReadCounts.csv"))
 
 ##############################################################
 # RSEM counts
 ##############################################################
 RSEMPath <- "/home/lima/Project/simulation/Comparision/RSEM"
-files <- file.path(RSEMPath, "Real", samples$GeneID, "genes.results")
-names(files) <- samples$GeneID
+files <- file.path(RSEMPath, "Real", high$GeneID, "genes.results")
+names(files) <- high$GeneID
 
 txi.rsem <- tximport(files, type = "rsem", txIn = FALSE, txOut = FALSE)
+
 head(txi.rsem)
 txi.rsem$length <- txi.rsem$length + 0.5
+# txi.rsem$counts <- log2(txi.rsem$counts + 1)
 # create DESeqDataSet object from tximport, ~1 indicating we are simply exploring the data
-dds <- DESeqDataSetFromTximport(txi.rsem, samples, ~1)
+# countData <- round(txi.rsem[['counts']])
+# colData(txi.rsem)
+dds <- DESeqDataSetFromTximport(txi.rsem, high, ~1)
 
+nrow(dds)
+# dds[rowSums(counts(dds)) > 2.5, 0]
 # rld <- varianceStabilizingTransformation(dds, blind = FALSE) # varianceStabilizingTransformation is not required due to it normalizes the coutns
 # rld_df <- data.frame(assay(rld))
-mcols(dds) <- DataFrame(mcols(dds))
+# mcols(dds) <- DataFrame(mcols(dds))
 #rld <- rld[rowSums(counts(rld)) > 2.5]
 cutOff = 0
 
 while(cutOff <= 10) {
-  # filtered <- rld[rowSums(assay(rld)) >= cutOff, ]
-  # keep <- rowSums(counts(rld)) >= cutOff
-  # output <- rld_df[keep, ]
-  output <- dds[rowSums(counts(dds)) >= cutOff]
+  keep <- rowSums(counts(dds)) >= cutOff
+  output <- dds[keep, ]
   output <- data.frame(assay(output))
   outPutName = paste("CountsCutOffValue", cutOff, ".csv", sep = "")
   print(outPutName)
-  outPutPath = file.path(RSEMPath, "Real", outPutName)
+  outPutPath = file.path("/home/lima/Project/simulation/Comparision/high", outPutName)
+  write.csv(output, outPutPath)
+  cutOff = cutOff + 2.5
+}
+
+
+########################################################################################################################################################
+# not that raw
+high <- read.table("/home/lima/Project/simulation/Comparision/high.txt", header = TRUE)
+high
+
+###########################################################
+# kallisto
+###########################################################
+kallPath <- "/home/lima/Project/simulation/Comparision/Kall"
+txdb <- EnsDb.Hsapiens.v75
+df <- transcripts(txdb, return.type = "DataFrame")
+tx2gene <- df[, 8 : 7]  # tx ID, then gene ID
+
+###########################################################
+# kallisto TSV files for real data
+###########################################################
+files <- file.path(kallPath, "Real", high$GeneID, "abundance.tsv")
+names(files) <- high$GeneID
+# some problems with rhdf5 packages
+#txi.kall <- tximport(files, type = "kallisto", txOut = TRUE)
+txi.kall.tsv <- tximport(files, type = "kallisto", dropInfReps=TRUE, tx2gene = tx2gene, ignoreTxVersion = TRUE, ignoreAfterBar = TRUE)
+txi.kall.tsv$length <- txi.kall.tsv$length + 0.5
+# txi.kall.tsv$counts <- log2(txi.kall.tsv$counts + 1)
+head(txi.kall.tsv$counts)
+dds <- DESeqDataSetFromTximport(txi.kall.tsv, high, ~1)
+
+# performe a rlog/varianceStabilizingTransformation
+rld <- varianceStabilizingTransformation(dds, blind = FALSE)
+# type(rld)
+# convert counts to a dataframe and save it into csv
+rld_df <- data.frame(assay(rld))
+
+write.csv(rld_df, file.path("/home/lima/Project/simulation/Comparision/high/Normalized", "NormalizedKallistoReadCounts.csv"))
+
+##############################################################
+# RSEM counts
+##############################################################
+RSEMPath <- "/home/lima/Project/simulation/Comparision/RSEM"
+files <- file.path(RSEMPath, "Real", high$GeneID, "genes.results")
+names(files) <- high$GeneID
+
+txi.rsem <- tximport(files, type = "rsem", txIn = FALSE, txOut = FALSE)
+
+head(txi.rsem)
+txi.rsem$length <- txi.rsem$length + 0.5
+# txi.rsem$counts <- log2(txi.rsem$counts + 1)
+# create DESeqDataSet object from tximport, ~1 indicating we are simply exploring the data
+# countData <- round(txi.rsem[['counts']])
+# colData(txi.rsem)
+dds <- DESeqDataSetFromTximport(txi.rsem, high, ~1)
+
+nrow(dds)
+# dds[rowSums(counts(dds)) > 2.5, 0]
+rld <- varianceStabilizingTransformation(dds, blind = FALSE) # varianceStabilizingTransformation is not required due to it normalizes the coutns
+rld_df <- data.frame(assay(rld))
+# mcols(dds) <- DataFrame(mcols(dds))
+#rld <- rld[rowSums(counts(rld)) > 2.5]
+cutOff = 0
+
+while(cutOff <= 10) {
+  keep <- rowSums(rld_df) >= cutOff
+  output <- rld_df[keep, ]
+  #output <- data.frame(assay(output))
+  outPutName = paste("NormalizedCountsCutOffValue", cutOff, ".csv", sep = "")
+  print(outPutName)
+  outPutPath = file.path("/home/lima/Project/simulation/Comparision/high/Normalized", outPutName)
   write.csv(output, outPutPath)
   cutOff = cutOff + 2.5
 }
